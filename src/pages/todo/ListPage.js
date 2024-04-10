5cedb485035a2489f9798df5ada909435b531b19
yelo-o <flyordig@gmail.com>
2024-04-10 21:06:15 +0900

    경로에 '?' 이후에 나오는 querystring 값 이용하기 : useSearchParams()

--- src/pages/todo/ListPage.js
+++ src/pages/todo/ListPage.js
@@ -1,8 +1,17 @@
+import { useSearchParams } from "react-router-dom";
+
 const ListPage = () => {
+
+    const [queryParams] = useSearchParams()
+    
+    const page = queryParams.get("page") ? parseInt(queryParams.get("page")) : 1
+    const size = queryParams.get("size") ? parseInt(queryParams.get("size")) : 10
+
+
     return (
         <div className="p-4 w-full bg-white">
             <div className="text-3xl font-extrabold">
-                Todo List Page component
+                Todo List Page component {page} --- {size}
             </div>
         </div>
     );
