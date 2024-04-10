95cbe3a17047c8ddc4261c66a822181e0e45f749
yelo-o <flyordig@gmail.com>
2024-04-10 20:42:11 +0900

    todo/list 경로 처리 : 중첩 라우팅

---
+++ src/pages/todo/ListPage.js
@@ -0,0 +1,11 @@
+const ListPage = () => {
+    return (
+        <div className="p-4 w-full bg-white">
+            <div className="text-3xl font-extrabold">
+                Todo List Page component
+            </div>
+        </div>
+    );
+}
+
+export default ListPage;
--- src/router/root.js
+++ src/router/root.js
@@ -6,7 +6,8 @@ const Loading = <div>Loading...</div>;
 const Main = lazy(() => import("../pages/MainPage"));
 const About = lazy(() => import("../pages/AboutPage"));
 
-const TodoIndex = lazy(() => import("../pages/todo/IndexPage"))
+const TodoIndex = lazy(() => import("../pages/todo/IndexPage"));
+const TodoList = lazy(() => import("../pages/todo/ListPage"));
 
 const root = createBrowserRouter([
     {
@@ -14,12 +15,18 @@ const root = createBrowserRouter([
         element: <Suspense fallback={Loading}><Main/></Suspense>
     },
     {
-        path: "/about",
+        path: "about",
         element: <Suspense fallback={Loading}><About/></Suspense>
     },
     {
-        path: "/todo",
-        element: <Suspense fallback={Loading}><TodoIndex/></Suspense>
+        path: "todo",
+        element: <Suspense fallback={Loading}><TodoIndex/></Suspense>,
+        children: [
+            {
+                path: "list",
+                element: <Suspense fallback={Loading}><TodoList/></Suspense>
+            }
+        ]
     },
 ])
 
