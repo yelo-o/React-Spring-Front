e81ba6979ef9d1c7de6e3622c1158f928f65746b
yelo-o <flyordig@gmail.com>
2024-04-10 21:00:22 +0900

    경로 자체 값 이용 : useParams()

--- src/pages/todo/ReadPage.js
+++ src/pages/todo/ReadPage.js
@@ -1,7 +1,11 @@
+import { useParams } from "react-router-dom";
 const ReadPage = () => {
+
+    const {tno} = useParams()
+
     return (
         <div className="text-3xl font-extrabold">
-            Todo Read Page Component
+            Todo Read Page Component {tno}
         </div>
     );
 }
