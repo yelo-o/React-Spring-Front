96b69877eeaf175e86c39ec10b8d0320ed10da87
yelo-o <flyordig@gmail.com>
2024-04-10 17:38:45 +0900

    페이지 컴포넌트 및 지연로딩 추가 - MainPage ("/') - AboutPage("/about")

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
+import { RouterProvider } from "react-router-dom";
+import root from "./router/root";
+
 function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <RouterProvider router={root} />
   );
 }
 
---
+++ src/pages/MainPage.js
@@ -0,0 +1,9 @@
+const MainPage = () => {
+    return (
+        <div className=" text-3xl">
+            <div>MainPage</div>
+        </div>
+    );
+}
+
+export default MainPage;
