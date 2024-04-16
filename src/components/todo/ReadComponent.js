9ec67b2a7f2bb66d349df50d6cc0c47327c0b5d3
yelo-o <flyordig@gmail.com>
2024-04-16 23:10:25 +0900

    조회 데이터 화면에 보여주기

--- src/components/todo/ReadComponent.js
+++ src/components/todo/ReadComponent.js
@@ -20,10 +20,22 @@ const ReadComponent = ({tno}) => {
     }, [tno])
 
     return (
-        <div>
-
+        <div className="border-2 border-sky-200 mt-10 m-2 p-4">
+            {makeDiv('Tno', todo.tno)}
+            {makeDiv('Writer', todo.writer)}
+            {makeDiv('Title', todo.title)}
+            {makeDiv('Due Date', todo.dueDate)}
+            {makeDiv('Complete', todo.complete ? 'Completed' : 'Not Yet')}
         </div>
     );
 }
+
+const makeDiv = (title, value) => 
+<div className="flex justify-center">
+    <div className="relative mb-4 flex w-full flex-wrap items-stretch">
+        <div className="w-1/5 p-6 text-right font-bold">{title}</div>
+        <div className="w-4/5 p-6 rounded-r border border-solid shadow-md">{value}</div>
+    </div>
+</div>
 
 export default ReadComponent;
